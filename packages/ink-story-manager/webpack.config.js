018de1a9480f3deb38fd6d74453855d1d6acb7b3
Francisco Fornell <fco.fornell@gmail.com>
2020-09-07 11:23:13 +0200

    build: :building_construction: Configure webpack minimizing and source maps

--- packages/ink-story-manager/webpack.config.js
+++ packages/ink-story-manager/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
+  devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'ink-story-manager.js',
