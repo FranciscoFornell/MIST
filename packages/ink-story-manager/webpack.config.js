b3c771235aac0799ed539e1400b5061a65c19f85
Francisco Fornell <fco.fornell@gmail.com>
2020-09-07 12:50:58 +0200

    build: :building_construction: Add build script for production deployment

--- packages/ink-story-manager/webpack.config.js
+++ packages/ink-story-manager/webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
+  plugins: [new CleanWebpackPlugin()],
   devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
