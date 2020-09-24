415abd23fe4ace032a8dcf7e06f928ba0dd09034
Francisco Fornell <fco.fornell@gmail.com>
2020-09-24 12:37:37 +0200

    build: :construction_worker: Configure simple-ink-story-web-player package

--- packages/simple-ink-story-web-player/babel.config.js
+++ packages/simple-ink-story-web-player/babel.config.js
@@ -2,13 +2,13 @@ module.exports = {
   presets: [
     [
       '@babel/preset-env',
-      '@babel/preset-react',
       {
         targets: {
           node: 'current',
         },
       },
     ],
+    '@babel/preset-react',
   ],
   plugins: ['@babel/plugin-proposal-class-properties'],
 };
