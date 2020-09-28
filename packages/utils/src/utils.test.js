9e0175b1de2bf788ca7b72ae3b2caca4ae8c3713
Francisco Fornell <fco.fornell@gmail.com>
2020-09-28 12:17:23 +0200

    feat: :sparkles: add pick method to utils package
    Add pick method to utils package

--- packages/utils/src/utils.test.js
+++ packages/utils/src/utils.test.js
@@ -1,5 +1,13 @@
-describe('Utils', () => {
-  it('should have tests', () => {
-    expect(true).toBe(true);
+import { pick } from './utils';
+
+describe('pick', () => {
+  const testObject = { a: 1, b: 2, c: 3 };
+
+  it('should return an object with the whitlisted given properties of the given object', () => {
+    expect(pick(testObject, 'a', 'b')).toEqual({ a: 1, b: 2 });
+  });
+
+  it('should not add undefined properties to the result', () => {
+    expect(pick(testObject, 'z')).toEqual({});
   });
 });
